a4c050c6fd6e0d416797537dd6900be94462a832
1xtend <vlad.mezhevyi@gmail.com>
2023-03-17 14:31:16 +0200

    about and work sections

--- js/script.js
+++ js/script.js
@@ -2,8 +2,18 @@
 
 const burger = document.querySelector('.burger');
 const navbar = document.querySelector('.navbar');
+const body = document.body;
 
 burger.addEventListener('click', (e) => {
   burger.classList.toggle('is-active');
   navbar.classList.toggle('is-active');
+  body.classList.toggle('is-fixed');
+
+  navbar.addEventListener('click', (e) => {
+    if (e.target !== e.currentTarget) {
+      burger.classList.remove('is-active');
+      navbar.classList.remove('is-active');
+      body.classList.remove('is-fixed');
+    }
+  });
 });
